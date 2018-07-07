90e6a2ba18673208648d16c6dd60cda7144bb0be
blee <brianlee2795@gmail.com>
2018-07-07 17:26:50 -0400

    User model created

--- aidisson/users/models.py
+++ aidisson/users/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Trainee(models.Model):
+    user            = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Trainee object is also related to dish_log, exercise_log, sleep_log, and weight_log
+
+    activated       = models.BooleanField(default=False)
+    join_date       = models.DateTimeField(auto_now_add=True)
+ 
