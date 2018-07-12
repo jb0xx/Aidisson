8b258867f5b409c15e8371a6fc4f64c447094679
blee <brianlee2795@gmail.com>
2018-07-11 22:23:43 -0400

    added Session and Cycle classes

--- aidisson/sleep/models.py
+++ aidisson/sleep/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+from users.models import Trainee
+
 # Create your models here.
+class Session(models.Model):
+    trainee         = models.ForeignKey(Trainee, on_delete=models.CASCADE)
+    starttime       = models.DateTimeField(null=True)
+    endtime         = models.DateTimeField(null=True)
+    quality         = models.FloatField(null=True)
+
+    def __str__(self):
+        user = self.trainee.user.username
+        date = self.starttime.date()
+        return f"{user}: {date}"
+    # def get_duration(self):
+    # def set_starttime(self):
+    # def set_endtime(self):
+    # def get_quality(self):
+    # def set_quality(self):
+
+
+
+class Cycle(models.Model):
+    # Constants
+    AWAKE = 'Awake'
+    LIGHT1 = 'NREM1'
+    LIGHT2 = 'NREM2'
+    DEEP = 'NREM3'
+    REM = 'REM'
+    SLEEP_STAGES = (
+        (AWAKE, 'Awake'),
+        (LIGHT1, 'Light1'),
+        (LIGHT2, 'Light2'),
+        (DEEP, 'Deep'),
+        (REM, 'REM'),
+    )
+
+    # Variables
+    session         = models.ForeignKey(Session, on_delete=models.CASCADE)
+    starttime       = models.DateTimeField()
+    endtime         = models.DateTimeField(auto_now_add=True)
+    stage           = models.CharField(
+                        max_length=5,
+                        choices=SLEEP_STAGES,
+                        default=LIGHT1)
--- aidisson/users/models.py
+++ aidisson/users/models.py
@@ -8,7 +8,16 @@ User = settings.AUTH_USER_MODEL
 class Trainee(models.Model):
     user            = models.OneToOneField(User, on_delete=models.CASCADE)
     # Trainee object is also related to dish_log, exercise_log, sleep_log, and weight_log
-
+    
     activated       = models.BooleanField(default=False)
     join_date       = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+    	return self.get_username()
+
+    def get_user(self):
+    	return self.user
+
+    def get_username(self):
+    	return self.user.username
  
