ce182aec3ef41f2fa11fac5296944f431713330c
blee <brianlee2795@gmail.com>
2018-07-26 22:02:55 -0400

    Dish modeladmin and save function

--- aidisson/dish/admin.py
+++ aidisson/dish/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Dish, Ingredient
 
-admin.site.register(Dish)
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+	readonly_fields = ('carb_total', 'fat_total', 'protein_total')
+
+
+
 admin.site.register(Ingredient)
--- aidisson/dish/models.py
+++ aidisson/dish/models.py
@@ -6,11 +6,13 @@ from django.db import models
 
 class Ingredient(models.Model):
 	name 			= models.CharField(max_length=30)
-	carb_comp		= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='carbs per 100g')
-	fat_comp		= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='fat per 100g')
-	protein_comp	= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='protein per 100g')
-	water_comp		= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='water per 100g')
-	serving_size	= models.DecimalField(max_digits=7, decimal_places=3, default=0, help_text='recommended serving size for this food in grams')
+	carb_comp		= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='grams of carbs per 100g')
+	fat_comp		= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='grams of fat per 100g')
+	fiber_comp		= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='grams of fiber per 100g')
+	protein_comp	= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='grams of protein per 100g')
+	sugar_comp		= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='grams of sugar per 100g')
+	water_comp		= models.DecimalField(max_digits=5, decimal_places=3, default=0, help_text='grams of water per 100g')
+	serving_size	= models.DecimalField(max_digits=7, decimal_places=3, default=0, help_text='recommended serving size for this ingredient in grams')
 
 
 	def __str__(self):
@@ -29,3 +31,20 @@ class Dish(models.Model):
 
 	def __str__(self):
 		return self.name
+
+
+	def save(self, *args, **kwargs):
+		super(Dish, self).save(*args, **kwargs)
+		serving_list = self.num_servings.split(',')
+		self.carb_total = 0
+		self.fat_total = 0
+		self.protein_total = 0
+		for n, i in zip(serving_list, self.ingredients.all()):
+			self.carb_total += int(n) * i.carb_comp * i.serving_size / 100
+			self.fat_total += int(n) * i.fat_comp * i.serving_size / 100
+			self.protein_total += int(n) * i.protein_comp * i.serving_size / 100
+		super(Dish, self).save(*args, **kwargs)
+
+
+	class Meta:
+		verbose_name_plural = 'dishes'
